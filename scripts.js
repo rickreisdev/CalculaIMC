ca8c7e05432f7781142be08f44097d2839f4833d
Rick <ricleyreis@gmail.com>
2023-12-08 16:58:40 -0300

    adding footer, change ilustrations

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('form')
 
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', function (event) {
     event.preventDefault()
 
     const peso = document.getElementById('weight').value
@@ -10,31 +10,60 @@ form.addEventListener('submit', function(event){
 
     const value = document.getElementById('value')
     const desc = document.getElementById('description')
+    const ilus = document.getElementById('ilus')
+    const ilus2 = document.getElementById('ilus2')
+    const ilus3 = document.getElementById('ilus3')
     let description = '';
 
+    ilus.classList.remove('hidden')
     value.classList.add('attention')
     desc.classList.add('attention')
 
     document.getElementById('infos').classList.remove('hidden')
 
-    if(imc < 18.5){
+    if (imc < 18.5) {
         description = "Cuidado! Você pode estar abaixo do peso!"
-    }else if(imc >= 18.5 && imc <= 25){
+        ilus.classList.add('hidden')
+        ilus3.classList.add('hidden')
+        ilus2.classList.remove('hidden')
+
+    } else if (imc >= 18.5 && imc <= 25) {
         description = "Você está no peso ideal!"
+        ilus.classList.remove('hidden')
+        ilus2.classList.add('hidden')
+        ilus3.classList.add('hidden')
+
         value.classList.remove('attention')
         value.classList.add('normal')
+
         desc.classList.remove('attention')
         desc.classList.add('normal')
-    }else if(imc >= 25 && imc <= 30){
+
+    } else if (imc >= 25 && imc <= 30) {
         description = "Cuidado! Você pode estar com sobrepeso!"
-    }else if(imc >= 30 && imc <= 35){
+        ilus.classList.remove('hidden')
+        ilus2.classList.add('hidden')
+        ilus3.classList.add('hidden')
+
+    } else if (imc >= 30 && imc <= 35) {
         description = "Cuidado! Você pode estar com obesidade moderada!"
-    }else if(imc >= 35 && imc <= 40){
+        ilus.classList.add('hidden')
+        ilus3.classList.add('hidden')
+        ilus2.classList.remove('hidden')
+
+    } else if (imc >= 35 && imc <= 40) {
         description = "Cuidado! Você pode estar com obesidade severa!"
-    }else{
+        ilus.classList.add('hidden')
+        ilus3.classList.add('hidden')
+        ilus2.classList.remove('hidden')
+
+    } else {
         description = "Cuidado! Você pode estar com obesidade mórbida!"
+        ilus.classList.add('hidden')
+        ilus2.classList.add('hidden')
+        ilus3.classList.remove('hidden')
     }
-    
+
     value.textContent = imc.replace('.', ',')
     document.getElementById('description').textContent = description
 })
